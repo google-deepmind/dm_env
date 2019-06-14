742b8b4aca7cf2271652177781cf679306ff986a
DeepMind <noreply@google.com>
2019-06-14 11:49:57 +0100

    Initial commit
    PiperOrigin-RevId: 253201735
    Change-Id: I808ef66e2b0b3aba043b98a5f7d72fe3ea07c08c

---
+++ dm_env/_environment.py
@@ -0,0 +1,240 @@
+# pylint: disable=g-bad-file-header
+# Copyright 2019 The dm_env Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Python RL Environment API."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import collections
+
+from dm_env import specs
+import enum
+import six
+
+
+class TimeStep(
+    collections.namedtuple('TimeStep',
+                           ['step_type', 'reward', 'discount', 'observation'])):
+  """Returned with every call to `step` and `reset` on an environment.
+
+  A `TimeStep` contains the data emitted by an environment at each step of
+  interaction. A `TimeStep` holds a `step_type`, an `observation` (typically a
+  NumPy array or a dict or list of arrays), and an associated `reward` and
+  `discount`.
+
+  The first `TimeStep` in a sequence will have `StepType.FIRST`. The final
+  `TimeStep` will have `StepType.LAST`. All other `TimeStep`s in a sequence will
+  have `StepType.MID.
+
+  Attributes:
+    step_type: A `StepType` enum value.
+    reward: A scalar, or `None` if `step_type` is `StepType.FIRST`, i.e. at the
+      start of a sequence.
+    discount: A discount value in the range `[0, 1]`, or `None` if `step_type`
+      is `StepType.FIRST`, i.e. at the start of a sequence.
+    observation: A NumPy array, or a nested dict, list or tuple of arrays.
+      Scalar values that can be cast to NumPy arrays (e.g. Python floats) are
+      also valid in place of a scalar array.
+  """
+  __slots__ = ()
+
+  def first(self):
+    # type: () -> bool
+    return self.step_type == StepType.FIRST
+
+  def mid(self):
+    # type: () -> bool
+    return self.step_type == StepType.MID
+
+  def last(self):
+    # type: () -> bool
+    return self.step_type == StepType.LAST
+
+
+class StepType(enum.IntEnum):
+  """Defines the status of a `TimeStep` within a sequence."""
+  # Denotes the first `TimeStep` in a sequence.
+  FIRST = 0
+  # Denotes any `TimeStep` in a sequence that is not FIRST or LAST.
+  MID = 1
+  # Denotes the last `TimeStep` in a sequence.
+  LAST = 2
+
+  def first(self):
+    # type: () -> bool
+    return self is StepType.FIRST
+
+  def mid(self):
+    # type: () -> bool
+    return self is StepType.MID
+
+  def last(self):
+    # type: () -> bool
+    return self is StepType.LAST
+
+
+@six.add_metaclass(abc.ABCMeta)
+class Base(object):
+  """Abstract base class for Python RL environments.
+
+  Observations and valid actions are described with `Array` specs, defined in
+  the `specs` module.
+  """
+
+  @abc.abstractmethod
+  def reset(self):
+    # type: () -> TimeStep
+    """Starts a new sequence and returns the first `TimeStep` of this sequence.
+
+    Returns:
+      A `TimeStep` namedtuple containing:
+        step_type: A `StepType` of `FIRST`.
+        reward: `None`, indicating the reward is undefined.
+        discount: `None`, indicating the discount is undefined.
+        observation: A NumPy array, or a nested dict, list or tuple of arrays.
+          Scalar values that can be cast to NumPy arrays (e.g. Python floats)
+          are also valid in place of a scalar array. Must conform to the
+          specification returned by `observation_spec()`.
+    """
+
+  @abc.abstractmethod
+  def step(self, action):
+    # type: (...) -> TimeStep
+    """Updates the environment according to the action and returns a `TimeStep`.
+
+    If the environment returned a `TimeStep` with `StepType.LAST` at the
+    previous step, this call to `step` will start a new sequence and `action`
+    will be ignored.
+
+    This method will also start a new sequence if called after the environment
+    has been constructed and `reset` has not been called. Again, in this case
+    `action` will be ignored.
+
+    Args:
+      action: A NumPy array, or a nested dict, list or tuple of arrays
+        corresponding to `action_spec()`.
+
+    Returns:
+      A `TimeStep` namedtuple containing:
+        step_type: A `StepType` value.
+        reward: Reward at this timestep, or None if step_type is
+          `StepType.FIRST`.
+        discount: A discount in the range [0, 1], or None if step_type is
+          `StepType.FIRST`.
+        observation: A NumPy array, or a nested dict, list or tuple of arrays.
+          Scalar values that can be cast to NumPy arrays (e.g. Python floats)
+          are also valid in place of a scalar array. Must conform to the
+          specification returned by `observation_spec()`.
+    """
+
+  def reward_spec(self):
+    """Describes the reward returned by the environment.
+
+    By default this is assumed to be a single float.
+
+    Returns:
+      An `Array` spec, or a nested dict, list or tuple of `Array` specs.
+    """
+    return specs.Array(shape=(), dtype=float, name='reward')
+
+  def discount_spec(self):
+    """Describes the discount returned by the environment.
+
+    By default this is assumed to be a single float between 0 and 1.
+
+    Returns:
+      An `Array` spec, or a nested dict, list or tuple of `Array` specs.
+    """
+    return specs.BoundedArray(
+        shape=(), dtype=float, minimum=0., maximum=1., name='discount')
+
+  @abc.abstractmethod
+  def observation_spec(self):
+    """Defines the observations provided by the environment.
+
+    May use a subclass of `specs.Array` that specifies additional properties
+    such as min and max bounds on the values.
+
+    Returns:
+      An `Array` spec, or a nested dict, list or tuple of `Array` specs.
+    """
+
+  @abc.abstractmethod
+  def action_spec(self):
+    """Defines the actions that should be provided to `step`.
+
+    May use a subclass of `specs.Array` that specifies additional properties
+    such as min and max bounds on the values.
+
+    Returns:
+      An `Array` spec, or a nested dict, list or tuple of `Array` specs.
+    """
+
+  def close(self):
+    """Frees any resources used by the environment.
+
+    Implement this method for an environment backed by an external process.
+
+    This method be used directly
+
+    ```python
+    env = Env(...)
+    # Use env.
+    env.close()
+    ```
+
+    or via a context manager
+
+    ```python
+    with Env(...) as env:
+      # Use env.
+    ```
+    """
+    pass
+
+  def __enter__(self):
+    """Allows the environment to be used in a with-statement context."""
+    return self
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    """Allows the environment to be used in a with-statement context."""
+    del exc_type, exc_value, traceback  # Unused.
+    self.close()
+
+
+# Helper functions for creating TimeStep namedtuples with default settings.
+
+
+def restart(observation):
+  """Returns a `TimeStep` with `step_type` set to `StepType.FIRST`."""
+  return TimeStep(StepType.FIRST, None, None, observation)
+
+
+def transition(reward, observation, discount=1.0):
+  """Returns a `TimeStep` with `step_type` set to `StepType.MID`."""
+  return TimeStep(StepType.MID, reward, discount, observation)
+
+
+def termination(reward, observation):
+  """Returns a `TimeStep` with `step_type` set to `StepType.LAST`."""
+  return TimeStep(StepType.LAST, reward, 0.0, observation)
+
+
+def truncation(reward, observation, discount=1.0):
+  """Returns a `TimeStep` with `step_type` set to `StepType.LAST`."""
+  return TimeStep(StepType.LAST, reward, discount, observation)
---
+++ dm_env/specs.py
@@ -0,0 +1,284 @@
+# pylint: disable=g-bad-file-header
+# Copyright 2019 The dm_env Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Classes that describe numpy arrays."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+
+class Array(object):
+  """Describes a numpy array or scalar shape and dtype.
+
+  An `Array` spec allows an API to describe the arrays that it accepts or
+  returns, before that array exists.
+  The equivalent version describing a `tf.Tensor` is `TensorSpec`.
+  """
+  __slots__ = ('_shape', '_dtype', '_name')
+  __hash__ = None
+
+  def __init__(self, shape, dtype, name=None):
+    """Initializes a new `Array` spec.
+
+    Args:
+      shape: An iterable specifying the array shape.
+      dtype: numpy dtype or string specifying the array dtype.
+      name: Optional string containing a semantic name for the corresponding
+        array. Defaults to `None`.
+
+    Raises:
+      TypeError: If `shape` is not an iterable of elements convertible to int,
+      or if `dtype` is not convertible to a numpy dtype.
+    """
+    self._shape = tuple(int(dim) for dim in shape)
+    self._dtype = np.dtype(dtype)
+    self._name = name
+
+  @property
+  def shape(self):
+    """Returns a `tuple` specifying the array shape."""
+    return self._shape
+
+  @property
+  def dtype(self):
+    """Returns a numpy dtype specifying the array dtype."""
+    return self._dtype
+
+  @property
+  def name(self):
+    """Returns the name of the Array."""
+    return self._name
+
+  def __repr__(self):
+    return 'Array(shape={}, dtype={}, name={})'.format(self.shape,
+                                                       repr(self.dtype),
+                                                       repr(self.name))
+
+  def __eq__(self, other):
+    """Checks if the shape and dtype of two specs are equal."""
+    if not isinstance(other, Array):
+      return False
+    return self.shape == other.shape and self.dtype == other.dtype
+
+  def __ne__(self, other):
+    return not self == other
+
+  def _fail_validation(self, message, *args):
+    message %= args
+    if self.name:
+      message += ' for spec %s' % self.name
+    raise ValueError(message)
+
+  def validate(self, value):
+    """Checks if value conforms to this spec.
+
+    Args:
+      value: a numpy array or value convertible to one via `np.asarray`.
+
+    Returns:
+      value, converted if necessary to a numpy array.
+
+    Raises:
+      ValueError: if value doesn't conform to this spec.
+    """
+    value = np.asarray(value)
+    if value.shape != self.shape:
+      self._fail_validation(
+          'Expected shape %r but found %r', self.shape, value.shape)
+    if value.dtype != self.dtype:
+      self._fail_validation(
+          'Expected dtype %s but found %s', self.dtype, value.dtype)
+    return value
+
+  def generate_value(self):
+    """Generate a test value which conforms to this spec."""
+    return np.zeros(shape=self.shape, dtype=self.dtype)
+
+  def __reduce__(self):
+    return Array, (self._shape, self._dtype, self._name)
+
+
+class BoundedArray(Array):
+  """An `Array` spec that specifies minimum and maximum values.
+
+  Example usage:
+  ```python
+  # Specifying the same minimum and maximum for every element.
+  spec = BoundedArray((3, 4), np.float64, minimum=0.0, maximum=1.0)
+
+  # Specifying a different minimum and maximum for each element.
+  spec = BoundedArray(
+      (2,), np.float64, minimum=[0.1, 0.2], maximum=[0.9, 0.9])
+
+  # Specifying the same minimum and a different maximum for each element.
+  spec = BoundedArray(
+      (3,), np.float64, minimum=-10.0, maximum=[4.0, 5.0, 3.0])
+  ```
+
+  Bounds are meant to be inclusive. This is especially important for
+  integer types. The following spec will be satisfied by arrays
+  with values in the set {0, 1, 2}:
+  ```python
+  spec = BoundedArray((3, 4), np.int, minimum=0, maximum=2)
+  ```
+  """
+  __slots__ = ('_minimum', '_maximum')
+  __hash__ = None
+
+  def __init__(self, shape, dtype, minimum, maximum, name=None):
+    """Initializes a new `BoundedArray` spec.
+
+    Args:
+      shape: An iterable specifying the array shape.
+      dtype: numpy dtype or string specifying the array dtype.
+      minimum: Number or sequence specifying the minimum element bounds
+        (inclusive). Must be broadcastable to `shape`.
+      maximum: Number or sequence specifying the maximum element bounds
+        (inclusive). Must be broadcastable to `shape`.
+      name: Optional string containing a semantic name for the corresponding
+        array. Defaults to `None`.
+
+    Raises:
+      ValueError: If `minimum` or `maximum` are not broadcastable to `shape`.
+      TypeError: If the shape is not an iterable or if the `dtype` is an invalid
+        numpy dtype.
+    """
+    super(BoundedArray, self).__init__(shape, dtype, name)
+
+    try:
+      np.broadcast_to(minimum, shape=shape)
+    except ValueError as numpy_exception:
+      raise ValueError('minimum is not compatible with shape. '
+                       'Message: {!r}.'.format(numpy_exception))
+
+    try:
+      np.broadcast_to(maximum, shape=shape)
+    except ValueError as numpy_exception:
+      raise ValueError('maximum is not compatible with shape. '
+                       'Message: {!r}.'.format(numpy_exception))
+
+    self._minimum = np.array(minimum)
+    self._minimum.setflags(write=False)
+
+    self._maximum = np.array(maximum)
+    self._maximum.setflags(write=False)
+
+  @property
+  def minimum(self):
+    """Returns a NumPy array specifying the minimum bounds (inclusive)."""
+    return self._minimum
+
+  @property
+  def maximum(self):
+    """Returns a NumPy array specifying the maximum bounds (inclusive)."""
+    return self._maximum
+
+  def __repr__(self):
+    template = ('BoundedArray(shape={}, dtype={}, name={}, '
+                'minimum={}, maximum={})')
+    return template.format(self.shape, repr(self.dtype), repr(self.name),
+                           self._minimum, self._maximum)
+
+  def __eq__(self, other):
+    if not isinstance(other, BoundedArray):
+      return False
+    return (super(BoundedArray, self).__eq__(other) and
+            (self.minimum == other.minimum).all() and
+            (self.maximum == other.maximum).all())
+
+  def validate(self, value):
+    value = np.asarray(value)
+    super(BoundedArray, self).validate(value)
+    if (value < self.minimum).any() or (value > self.maximum).any():
+      self._fail_validation(
+          'Values were not all within bounds %s <= value <= %s',
+          self.minimum, self.maximum)
+    return value
+
+  def generate_value(self):
+    return (np.ones(shape=self.shape, dtype=self.dtype) *
+            self.dtype.type(self.minimum))
+
+  def __reduce__(self):
+    return BoundedArray, (self._shape, self._dtype, self._minimum,
+                          self._maximum, self._name)
+
+
+_NUM_VALUES_NOT_POSITIVE = '`num_values` must be a positive integer, got {}.'
+_DTYPE_NOT_INTEGRAL = '`dtype` must be integral, got {}.'
+_DTYPE_OVERFLOW = (
+    '`dtype` {} is not big enough to hold `num_values` ({}) without overflow.')
+
+
+class DiscreteArray(BoundedArray):
+  """Represents a discrete, scalar, zero-based space.
+
+  Concretely this is a 0-dimensional numpy array containing a single integer
+  value between 0 and num_items - 1 (inclusive).
+  """
+
+  _REPR_TEMPLATE = (
+      'DiscreteArray(shape={self.shape}, dtype={self.dtype}, name={self.name}, '
+      'minimum={self.minimum}, maximum={self.maximum}, '
+      'num_values={self.num_values})')
+
+  __slots__ = ('_num_values',)
+
+  def __init__(self, num_values, dtype=np.int32, name=None):
+    """Initializes a new `DiscreteArray` spec.
+
+    Args:
+      num_values: Integer specifying the number of possible values to represent.
+      dtype: The dtype of the array. Must be an integral type large enough to
+        hold `num_values` without overflow.
+      name: Optional string specifying the name of the array.
+
+    Raises:
+      ValueError: If `num_values` is not positive, if `dtype` is not integral,
+        or if `dtype` is not large enough to hold `num_values` without overflow.
+    """
+    if num_values <= 0 or not np.issubdtype(type(num_values), np.integer):
+      raise ValueError(_NUM_VALUES_NOT_POSITIVE.format(num_values))
+
+    if not np.issubdtype(dtype, np.integer):
+      raise ValueError(_DTYPE_NOT_INTEGRAL.format(dtype))
+
+    num_values = int(num_values)
+    maximum = num_values - 1
+    dtype = np.dtype(dtype)
+
+    if np.min_scalar_type(maximum) > dtype:
+      raise ValueError(_DTYPE_OVERFLOW.format(dtype, num_values))
+
+    super(DiscreteArray, self).__init__(
+        shape=(),
+        dtype=dtype,
+        minimum=0,
+        maximum=maximum,
+        name=name)
+    self._num_values = num_values
+
+  @property
+  def num_values(self):
+    """Returns the number of items."""
+    return self._num_values
+
+  def __repr__(self):
+    return self._REPR_TEMPLATE.format(self=self)
+
+  def __reduce__(self):
+    return DiscreteArray, (self._num_values, self._dtype, self._name)
